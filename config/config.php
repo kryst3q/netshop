63c28252f7e0cb3c16270fac6871322afa5cab3d
Krystian Kulesz <krystian@student.edu>
2017-05-30 00:59:50 +0200

    Create config dir & config DB dataset file

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$connData = [
+    'host'      => 'localhost',
+    'username'  => 'root',
+    'password'  => 'coderslab',
+    'db'        => 'netshop'
+];
+
+Connection::getConnectionData($connData);
