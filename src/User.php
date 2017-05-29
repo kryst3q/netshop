089bea41e9863d3cd0fc060c6f23dfbbb47d6b51
Krystian Kulesz <krystian@student.edu>
2017-05-30 01:06:49 +0200

    Create User class; implementations: attributes, getters&setters+validations, createUser func

---
+++ src/User.php
@@ -0,0 +1,138 @@
+<?php
+
+//require_once 'Connection.php';
+
+class User {
+    
+    public $id;
+    public $name;
+    public $surname;
+    public $email;
+    private $hashedPassword;
+    public $address;
+    public $active;
+    
+    public function __construct() {
+        $this->id = -1;
+        $this->name = "";
+        $this->surname = "";
+        $this->email = "";
+        $this->hashedPassword = "";
+        $this->address = "";
+        $this->active = TRUE;
+    }
+    
+    public function getId() : int {
+        return $this->id;
+    }
+
+    public function getName() : string {
+        return $this->name;
+    }
+
+    public function getSurname() : string {
+        return $this->surname;
+    }
+
+    public function getEmail() : string {
+        return $this->email;
+    }
+
+    public function getHashedPassword() : string {
+        return $this->hashedPassword;
+    }
+
+    public function getAddress() : string {
+        return $this->address;
+    }
+
+    public function getActive() {
+        return $this->active;
+    }
+
+    public function setId($id) : bool {
+        
+        if (is_int($id)) {
+            
+            $this->id = $id;
+            return TRUE;
+            
+        }
+        return FALSE;
+    }
+
+    public function setName($name) : bool {
+        
+        if (preg_match("/^[a-z]+$/i", $name)) {
+            
+            $this->name = $name;
+            return TRUE;
+            
+        }
+        return FALSE;
+    }
+
+    public function setSurname($surname) : bool {
+        
+        if (preg_match("/^[a-z]+$/i", $surname)) {
+            
+            $this->surname = $surname;
+            return TRUE;
+            
+        }
+        return FALSE;
+    }
+
+    public function setEmail($email) : bool {
+        
+        if (preg_match("/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]{1,})*\.([a-zA-Z]{2,}){1}$/", $email)) {
+            
+            $this->email = $email;
+            return TRUE;
+            
+        }
+        return FALSE;
+    }
+
+    public function setHashedPassword($password) {
+        $this->hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+    }
+
+    public function setAddress($address) : bool {
+        
+        if (preg_match("/^[a-z0-9 ,.\-]+$/i", $address)) {
+            
+            $this->address = $address;
+            return TRUE;
+            
+        }
+        return FALSE;
+    }
+
+    public function setActive($active) : bool {
+        
+        if (is_bool($active)) {
+            
+            $this->active = $active;
+            return TRUE;
+            
+        }
+        return FALSE;
+    }
+
+    public function createUser(User $user) {
+        
+        $query = "INSERT INTO users (email, hashed_password, name, surname, address, active) VALUES ('"
+                . $user->getEmail() . "', '"
+                . $user->getHashedPassword() . "', '"
+                . $user->getName() . "', '"
+                . $user->getSurname() . "', '"
+                . $user->getAddress() . "', "
+                . $user->getActive() . ")";
+        
+        return (Connection::connect($query)) ? TRUE : FALSE;
+        
+    }
+    
+}
+
